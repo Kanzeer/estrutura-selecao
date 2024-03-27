e039e63bc9740daa52e061bf78a1af77e4564638
Kanzeer <miguelroaamaral@gmail.com>
2024-03-27 17:21:40 -0400

    questão 3 finalizada

---
+++ questao3.js
@@ -0,0 +1,9 @@
+//Questão 3: Como você pode usar uma declaração if para verificar se uma variável é do tipo string em JavaScript?
+
+var termo = "testando";
+
+if(typeof termo === 'string') {
+    console.log("Isso é uma string")
+} else {
+    console.log("Isso não é uma string")
+}
