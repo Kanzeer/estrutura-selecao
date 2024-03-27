c6a2d935c4e850be7aea3fb701a99dcb2b2b8c1b
Kanzeer <miguelroaamaral@gmail.com>
2024-03-27 16:01:50 -0400

    Adicionado exemplo de estrutura de seleção encadeada

---
+++ selecao-encadeada.js
@@ -0,0 +1,9 @@
+let dia = "segunda";
+
+if(dia === "sábado") {
+    console.log("Parabéns! Agora descanse")
+} else if (dia === "domingo") {
+    console.log("Se prepare para domingo a noite..")
+} else {
+    console.log("Levanta e vai trabalha!")
+}
